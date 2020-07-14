32615cfee370299954a744bf85ef0c8ecccc8ea5
himabindu-bhoopalan <clickhima@gmail.com>
2020-07-14 19:17:33 +0530

    commit

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const saltRounds = 10
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //bus details updated by the operator
 app.put('/updatebus', function (req, res) {
@@ -738,6 +743,8 @@ app.post('/signup', function (req, res) {
 
 
 });             //app.post
+
+
 
 app.post('/signin', function (req, res) {
     console.log(req.body);
